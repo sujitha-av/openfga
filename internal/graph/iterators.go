b9877b9d58688ec03bb725582b2863932ae6bf92
José Padilla <jose.padilla@okta.com>
2024-10-04 13:18:08 -0400

    perf: remove duplicate data from cached records

---
+++ internal/graph/iterators.go
@@ -0,0 +1,75 @@
+package graph
+
+import (
+	"context"
+
+	openfgav1 "github.com/openfga/api/proto/openfga/v1"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/openfga/openfga/pkg/storage"
+)
+
+// cachedTupled is a simple struct to hold the minimal data needed
+// to store in a cached iterator.
+type cachedTuple struct {
+	user      string
+	condition *openfgav1.RelationshipCondition
+	timestamp *timestamppb.Timestamp
+}
+
+// cachedTupleIterator is a wrapper around a cached iterator
+// for a given object/relation.
+type cachedTupleIterator struct {
+	object   string
+	relation string
+	iter     storage.Iterator[cachedTuple]
+}
+
+var _ storage.TupleIterator = (*cachedTupleIterator)(nil)
+
+// Next will return the next available minimal cached tuple tuple
+// as a well-formed [openfgav1.Tuple].
+func (c *cachedTupleIterator) Next(ctx context.Context) (*openfgav1.Tuple, error) {
+	t, err := c.iter.Next(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	cachedTuple := &openfgav1.Tuple{
+		Key: &openfgav1.TupleKey{
+			User:      t.user,
+			Object:    c.object,
+			Relation:  c.relation,
+			Condition: t.condition,
+		},
+		Timestamp: t.timestamp,
+	}
+
+	return cachedTuple, nil
+}
+
+// Stop see [storage.Iterator].Stop
+func (c *cachedTupleIterator) Stop() {
+	c.iter.Stop()
+}
+
+// Head will return the first minimal cached tuple of the iterator as
+// a well-formed [openfgav1.Tuple].
+func (c *cachedTupleIterator) Head(ctx context.Context) (*openfgav1.Tuple, error) {
+	t, err := c.iter.Head(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	cachedTuple := &openfgav1.Tuple{
+		Key: &openfgav1.TupleKey{
+			User:      t.user,
+			Object:    c.object,
+			Relation:  c.relation,
+			Condition: t.condition,
+		},
+		Timestamp: t.timestamp,
+	}
+
+	return cachedTuple, nil
+}
